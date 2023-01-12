fc6f782d87d1287c11f776e4265c198bfc9c9ac8
Hemon <108059872+AkeGPrince@users.noreply.github.com>
2023-01-12 17:02:03 +0100

    Cours & Exos

--- Jonathan_exo/Streamlit/Insurance_streamlit/app/app.py
+++ Jonathan_exo/Streamlit/Insurance_streamlit/app/app.py
@@ -3,8 +3,6 @@ import numpy
 import streamlit as st
 
 
-# st.markdown("# Hemon GNAMBE \n ## Consultant Data Junior \n ##### Email: Simplonia3@gmail.com")
-# st.image('./data/avatar.png', caption="Myself", width=500 )
 
 st.markdown("<h1 style='text-align: center;'>Me, Myself & I : </h1>", unsafe_allow_html=True)
 
@@ -16,9 +14,9 @@ st.text("")
 
 col1, mid, col2 = st.columns([30,1,30])
 with col2:
-    st.image('./data/avatar.png', width=400)
+    #st.image('./data/avatar.png', width=400)
 with col1:
-    st.markdown("# Hemon GNAMBE \n ## Consultant Data Junior")
+    st.markdown("# H. Gxxxxx \n ## Consultant Data Junior")
     infos = st.button('Display my informations',)
     if infos:
         st.markdown("##### Contact: \n ###### Simplonia3@gmail.com \n 06.67.76.28.xx")
